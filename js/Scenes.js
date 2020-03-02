05ba12fb7cc8370a64485c9da262b705810a68c1
tom-nua <55702894+tom-nua@users.noreply.github.com>
2020-03-02 00:56:49 +0000

    Fixing scene load

---
+++ js/Scenes.js
@@ -0,0 +1,33 @@
+class BaseScene extends Phaser.Scene {
+
+    constructor(key){
+        super(key);
+    }
+
+    preload(){
+
+    }
+    create(){
+
+    }
+    update(){
+
+    }
+}
+
+class Level1 extends BaseScene {
+    
+    constructor(key){
+        super(key);
+    }
+
+    preload(){
+
+    }
+    create(){
+
+    }
+    update(){
+
+    }
+}
