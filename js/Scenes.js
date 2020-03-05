e0de174f9fa9c03361c68e85536d17ed58296ba2
tom-nua <55702894+tom-nua@users.noreply.github.com>
2020-03-05 14:49:47 +0000

    Enemy follows path :smile:

--- js/Scenes.js
+++ js/Scenes.js
@@ -12,12 +12,20 @@ class BaseScene extends Phaser.Scene {
         this.tilemap.createStaticLayer('background', tilesheet);
         this.tilemap.createStaticLayer('placeTiles', tilesheet);
 
-        this.pointsX = [251.00];
-        this.pointsY = [255.00];
+        this.spawnX = -18.00;
+        this.spawnY = 258.00;
 
-        this.enemies = this.physics.add.group();   
-                
-        this.spawnEnemy()
+        this.points = [
+            251.00, 255.00,
+            256.99, 132.15,
+            820.31, 149.13,
+            833.29, 446.77,
+            1298.73, 457.75
+        ];
+
+        this.enemies = this.physics.add.group();
+        
+        this.spawnEnemy();
         
     }
     update(){
@@ -26,39 +34,26 @@ class BaseScene extends Phaser.Scene {
 
     spawnEnemy(){
         //local enemy create
-        let newEnemy = this.add.follower(null, -18.00, 258.00, 'tilesheet', 245);
-        newEnemy.currentPoint = 0;
-        this.enemies.add(newEnemy);
 
         // new path
-        let path = new Phaser.Curves.Line(
-            new Phaser.Math.Vector2(
-                -18.00, 258.00
-            ), 
-            new Phaser.Math.Vector2(
-                this.pointsX[newEnemy.currentPoint], this.pointsY[newEnemy.currentPoint]
-            )
-        );
+        let path = new Phaser.Curves.Path(this.spawnX, this.spawnY);
+        path.splineTo(this.points);
 
-        newEnemy.path = path;
-        newEnemy.startFollow({duration: 1000, onComplete: function(){
-            console.log(this);
-            this.parent.scene.pathEnd(newEnemy);
-        }});
+        let newEnemy = this.add.follower(path, this.spawnX, this.spawnY, 'tilesheet', 245);
+        this.enemies.add(newEnemy);
+        
+        newEnemy.startFollow({
+            positionOnPath: true,
+            duration: 10000,
+            yoyo: false,
+            repeat: 0,
+            rotateToPath: true,
+            verticalAdjust: true
+        });
+
     }
 
-    pathEnd(enemy){
-        enemy.currentPoint =+ 1;
-        let newPath = new Phaser.Curves.Line(
-            origin, 
-            new Phaser.Math.Vector2(
-                this.pointsX[enemy.currentPoint], this.pointsY[enemy.currentPoint]
-            )
-        );
-
-        enemy.path = newPath;
-        enemy.startFollow({duration: 5000, onComplete: this.pathEnd});
-    }
+    
 }
 
 class Level1 extends BaseScene {
