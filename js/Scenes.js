2231c8ef66b0d8343d6de9b43d3ef0165b6c4354
tom-nua <55702894+tom-nua@users.noreply.github.com>
2020-03-07 22:36:48 +0000

    UI tile placement and started work on turret enemy detection

--- js/Scenes.js
+++ js/Scenes.js
@@ -36,23 +36,51 @@ class BaseScene extends Phaser.Scene {
         //debug spawn enemy
         this.spawnEnemy();
 
+        this.scene.run('UIScene',{gameScene: this});
+        this.UIScene = this.scene.get("UIScene");
+
+        this.selectedTile;
+
         // events
         this.input.on('pointerdown', function (pointer) {
             let scene = this.scene;
+            if(!scene.selectedTile){
+                return;
+            }
             let pointerX = pointer.worldX;
             let pointerY = pointer.worldY
-            let existingTile = scene.placeTileLayer.hasTileAtWorldXY(pointerX, pointerY);
-            if(existingTile){
-                let newTile = scene.placeTileLayer.putTileAtWorldXY(181, pointerX, pointerY);
-                let tileWorldPos = scene.placeTileLayer.tileToWorldXY(newTile.x, newTile.y);
-                let newDefence = scene.defences.create(tileWorldPos.x + 32, tileWorldPos.y + 25, 'tilesheet', 249);
+            let existingTile = scene.placeTileLayer.getTileAtWorldXY(pointerX, pointerY);
+            if (existingTile) {
+                scene.placeTileLayer.putTileAt(181, existingTile.x, existingTile.y);
+                let tileWorldPos = scene.placeTileLayer.tileToWorldXY(existingTile.x, existingTile.y);
+                let newDefence = scene.defences.create(tileWorldPos.x + 32, tileWorldPos.y + 25, 'tilesheet', scene.selectedTile);
+                scene.selectedTile = null;
+                scene.UIScene.unToggleButtons();
             }
-            
+
         });
     }
 
     update() {
+        for (const defence of this.defences.getChildren()) {
+            let nearestEnemy = this.findNearestEnemy(defence);
+            if (nearestEnemy) {
+                console.log("fire!");
+            }
+        };
+    }
 
+    findNearestEnemy(defence) {
+        for (const enemy of this.enemies.getChildren()) {
+            let distanceBetween = Phaser.Math.Distance.Between
+                (
+                    defence.x, defence.y,
+                    enemy.x, enemy.y
+                )
+            if (distanceBetween <= 200) {
+                return enemy;
+            }
+        };
     }
 
     spawnEnemy() {
@@ -85,7 +113,7 @@ class BaseScene extends Phaser.Scene {
 class Level1 extends BaseScene {
 
     constructor(key) {
-        super(key);
+        super('Level1');
     }
 
     preload() {
@@ -98,5 +126,54 @@ class Level1 extends BaseScene {
     }
     update() {
         super.update();
+    }
+}
+
+class UIScene extends Phaser.Scene {
+    constructor(key) {
+        super('UIScene');
+    }
+    init(data){
+        this.gameScene = data.gameScene;
+    }
+    preload() {
+        this.load.image('placementMenuBackground', 'assets/longMenu.png');
+        this.load.image('button', 'assets/button.png');
+        this.load.image('buttonPressed', 'assets/buttonPressed.png');
+    }
+    create() {
+        this.menuBackground = this.add.sprite(564, 576, 'placementMenuBackground');
+        this.menuBackground.scaleX = 2;
+        this.menuBackground.scaley = 2;
+        this.toggledButton = null;
+        this.defenceButton = this.add.sprite(560, 576, 'button');
+        this.defenceButton.setInteractive();
+        this.defenceButton.on('pointerdown', this.defencePressed);
+    }
+    defencePressed(){
+        if(!this.scene.gameScene.selectedTile){
+            this.scene.gameScene.selectedTile = 249;
+        }
+        else{
+            this.scene.gameScene.selectedTile = null;
+        }
+        this.scene.toggleButton(this);
+    }
+    toggleButton(button){
+        if (button.toggledOn){
+            this.toggledButton = null;
+            button.setTexture('button');
+        }
+        else{
+            this.toggledButton = button;
+            button.setTexture('buttonPressed');
+        }
+        button.toggledOn = !button.toggledOn;
+    }
+    unToggleButtons(){
+        this.toggleButton(this.toggledButton);
+    }
+    update() {
+
     }
 }
